2c6b822115bfc11b71c870de7ba7b56c21ef23eb
Tuomas <tuomakuh@metropolia.fi>
2022-03-29 11:54:19 +0300

    new idea UI

--- lib/ui/screens/new_idea_screen.dart
+++ lib/ui/screens/new_idea_screen.dart
@@ -10,6 +10,69 @@ class NewIdeaScreen extends StatefulWidget {
 class _NewIdeaScreenState extends State<NewIdeaScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Submit new idea")),
+      body: Container(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TextField(
+                decoration: InputDecoration(labelText: "Title"),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "Description", fillColor: Colors.grey[500]),
+                minLines: 3,
+                maxLines: 11,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Add photos",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.grey[200],
+                    height: 100,
+                    width: 100,
+                    child: const Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    height: 100,
+                    width: 100,
+                    child: const Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey[200],
+                    height: 100,
+                    width: 100,
+                    child: const Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              Align(alignment: Alignment.center, child: ElevatedButton(onPressed: () {}, child: const Text("Submit")),)
+              
+            ],
+          )),
+    );
   }
 }
