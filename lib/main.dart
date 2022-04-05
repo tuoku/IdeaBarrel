0e30b5b1d23dabba26fd6f8ca36d84d504b35ec4
Tuomas <tuomakuh@metropolia.fi>
2022-04-05 10:24:02 +0300

    new idea submission process

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:ideabarrel/ui/screens/leaderboard_screen.dart';
 import 'package:ideabarrel/ui/screens/new_idea_screen.dart';
 import 'package:ideabarrel/ui/screens/swipe_screen.dart';
 
+final routeObserver = RouteObserver<PageRoute>();
+final duration = const Duration(milliseconds: 300);
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +21,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        unselectedWidgetColor: Colors.white
       ),
+      navigatorObservers: [routeObserver],
       home: const Root(),
     );
   }
@@ -30,23 +36,42 @@ class Root extends StatefulWidget {
   State<Root> createState() => _RootState();
 }
 
-class _RootState extends State<Root> {
+class _RootState extends State<Root> with RouteAware {
   var currentPage = 0;
   Widget body = SwipeScreen();
+  GlobalKey _fabKey = GlobalKey();
+  bool _fabVisible = true;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    routeObserver.subscribe(this, ModalRoute.of(context) as PageRoute);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    routeObserver.unsubscribe(this);
+  }
+
+  @override
+  void didPopNext() {
+    super.didPopNext();
+    Timer(duration, () {
+      setState(() {
+        _fabVisible = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: body,
       floatingActionButton: currentPage == 0 || currentPage == 1
-          ? FloatingActionButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (c) {
-                  return const NewIdeaScreen();
-                }));
-              },
-              child: const Icon(Icons.add),
-            )
+          ? Visibility(
+              visible: _fabVisible,
+              child: _buildFAB(context, key: _fabKey))
           : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: currentPage == 0 || currentPage == 1
@@ -98,4 +123,90 @@ class _RootState extends State<Root> {
           : null,
     );
   }
+
+    Widget _buildFAB(context, {key}) => FloatingActionButton(
+        elevation: 0,
+        key: key,
+        onPressed: () => _onFabTap(context),
+        child: Icon(Icons.add),
+      );
+
+_onFabTap(BuildContext context) {
+    
+    // Hide the FAB on transition start
+    setState(() => _fabVisible = false);
+
+    final RenderBox fabRenderBox = _fabKey.currentContext?.findRenderObject() as RenderBox;
+    final fabSize = fabRenderBox.size;
+    final fabOffset = fabRenderBox.localToGlobal(Offset.zero);
+
+    Navigator.of(context).push(PageRouteBuilder(
+      transitionDuration: duration,
+      pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) =>
+          NewIdeaScreen(),
+      transitionsBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation, Widget child) =>
+          _buildTransition(child, animation, fabSize, fabOffset),
+    ));
+  }
+
+   Widget _buildTransition(
+    Widget page,
+    Animation<double> animation,
+    Size fabSize,
+    Offset fabOffset,
+  ) {
+    if (animation.value == 1) return page;
+
+    final borderTween = BorderRadiusTween(
+      begin: BorderRadius.circular(fabSize.width / 2),
+      end: BorderRadius.circular(0.0),
+    );
+    final sizeTween = SizeTween(
+      begin: fabSize,
+      end: MediaQuery.of(context).size,
+    );
+    final offsetTween = Tween<Offset>(
+      begin: fabOffset,
+      end: Offset.zero,
+    );
+
+    final easeInAnimation = CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeIn,
+    );
+    final easeAnimation = CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeOut,
+    );
+
+    final radius = borderTween.evaluate(easeInAnimation);
+    final offset = offsetTween.evaluate(animation);
+    final size = sizeTween.evaluate(easeInAnimation);
+
+    final transitionFab = Opacity(
+      opacity: 1 - easeAnimation.value,
+      child: _buildFAB(context),
+    );
+
+    Widget positionedClippedChild(Widget child) => Positioned(
+        width: size?.width,
+        height: size?.height,
+        left: offset.dx,
+        top: offset.dy,
+        child: ClipRRect(
+          borderRadius: radius,
+          child: child,
+        ));
+
+    return Stack(
+      children: [
+        positionedClippedChild(page),
+        positionedClippedChild(transitionFab),
+      ],
+    );
+  }
 }
+
+
--- lib/ui/screens/new_idea_screen.dart
+++ lib/ui/screens/new_idea_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:page_view_indicators/linear_progress_page_indicator.dart';
+import 'package:page_view_indicators/step_page_indicator.dart';
+import 'package:show_up_animation/show_up_animation.dart';
 
 class NewIdeaScreen extends StatefulWidget {
   const NewIdeaScreen({Key? key}) : super(key: key);
@@ -8,76 +15,454 @@ class NewIdeaScreen extends StatefulWidget {
 }
 
 class _NewIdeaScreenState extends State<NewIdeaScreen> {
+  final _pageController = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Submit new idea")),
-      body: Container(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const TextField(
-                decoration: InputDecoration(labelText: "Title"),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Container(
+      color: Colors.transparent,
+      padding: const EdgeInsets.all(16.0),
+      child: StepPageIndicator(
+        stepColor: Colors.white,
+        itemCount: 5,
+        currentPageNotifier: _currentPageNotifier,
+        size: 16,
+        onPageSelected: (int index) {
+          if (_currentPageNotifier.value > index)
+            _pageController.jumpToPage(index);
+        },
+      ),
+    ),
+
+          leading: IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: PageView(
+          onPageChanged: (int index) {
+            _currentPageNotifier.value = index;
+          },
+          controller: _pageController,
+          children: [
+            _TitlePage(),
+            _DescPage(),
+            _DeptPage(),
+            _PhotosPage(),
+            _SubmitPage(),
+          ],
+        ));
+
+  }
+}
+
+class _TitlePage extends StatefulWidget {
+  _TitlePage({Key? key}) : super(key: key);
+
+  @override
+  State<_TitlePage> createState() => __TitlePageState();
+}
+
+class __TitlePageState extends State<_TitlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color.fromARGB(255, 44, 143, 224),
+          Color.fromARGB(255, 18, 195, 226)
+        ]),
+      ),
+      padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+      child: Column(
+        children: [
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 0),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Text(
+                "Sum your idea up in a few words",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white),
+              )),
+          SizedBox(
+            height: 70,
+          ),
+          ShowUpAnimation(
+            delayStart: Duration(milliseconds: 100),
+            animationDuration: Duration(milliseconds: 600),
+            curve: Curves.easeIn,
+            direction: Direction.horizontal,
+            offset: -0.2,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Title",
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
               ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Description", fillColor: Colors.grey[500]),
-                minLines: 3,
-                maxLines: 11,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "Add photos",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    color: Colors.grey[200],
-                    height: 100,
-                    width: 100,
-                    child: const Icon(
-                      Icons.add_a_photo,
-                      size: 40,
-                    ),
-                  ),
-                  Container(
-                    color: Colors.grey[200],
-                    height: 100,
-                    width: 100,
-                    child: const Icon(
-                      Icons.add_a_photo,
-                      size: 40,
-                    ),
-                  ),
-                  Container(
-                    color: Colors.grey[200],
-                    height: 100,
-                    width: 100,
-                    child: const Icon(
-                      Icons.add_a_photo,
-                      size: 40,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                    onPressed: () {}, child: const Text("Submit")),
-              )
-            ],
-          )),
+            ),
+          )
+        ],
+      ),
     );
+  }
+}
+
+class _DescPage extends StatefulWidget {
+  _DescPage({Key? key}) : super(key: key);
+
+  @override
+  State<_DescPage> createState() => __DescPageState();
+}
+
+class __DescPageState extends State<_DescPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color.fromARGB(255, 44, 143, 224),
+          Color.fromARGB(255, 18, 195, 226)
+        ]),
+      ),
+      padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+      child: Column(
+        children: [
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 0),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Text(
+                "Tell everyone more about your idea",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white),
+              )),
+          SizedBox(
+            height: 70,
+          ),
+          ShowUpAnimation(
+            delayStart: Duration(milliseconds: 100),
+            animationDuration: Duration(milliseconds: 600),
+            curve: Curves.easeIn,
+            direction: Direction.horizontal,
+            offset: -0.2,
+            child: TextField(
+              maxLines: 10,
+              decoration: InputDecoration(
+                hintText: "Description",
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _DeptPage extends StatefulWidget {
+  _DeptPage({Key? key}) : super(key: key);
+
+  @override
+  State<_DeptPage> createState() => __DeptPageState();
+}
+
+class __DeptPageState extends State<_DeptPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color.fromARGB(255, 44, 143, 224),
+          Color.fromARGB(255, 18, 195, 226)
+        ]),
+      ),
+      padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+      child: Column(
+        children: [
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 0),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Text(
+                "Which department might be able to make your idea come true?",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white),
+              )),
+          SizedBox(
+            height: 70,
+          ),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 100),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: CheckboxListTile(
+                tileColor: Colors.white,
+                title: Text(
+                  "Workplace Resources",
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  "Responsible for physical improvements to the workplace",
+                  style: TextStyle(color: Colors.grey[300]),
+                ),
+                onChanged: (b) {},
+                value: false,
+              )),
+          SizedBox(
+            height: 40,
+          ),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 200),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: CheckboxListTile(
+                tileColor: Colors.white,
+                title: Text(
+                  "Human Resources",
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  "Responsible for the well-being of people",
+                  style: TextStyle(color: Colors.grey[300]),
+                ),
+                onChanged: (b) {},
+                value: false,
+              )),
+          SizedBox(
+            height: 40,
+          ),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 300),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: CheckboxListTile(
+                tileColor: Colors.white,
+                title: Text(
+                  "Site Team",
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  "Everything else",
+                  style: TextStyle(color: Colors.grey[300]),
+                ),
+                onChanged: (b) {},
+                value: false,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _PhotosPage extends StatefulWidget {
+  _PhotosPage({Key? key}) : super(key: key);
+
+  @override
+  State<_PhotosPage> createState() => __PhotosPageState();
+}
+
+class __PhotosPageState extends State<_PhotosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(colors: [
+          Color.fromARGB(255, 44, 143, 224),
+          Color.fromARGB(255, 18, 195, 226)
+        ]),
+      ),
+      padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+      child: Column(
+        children: [
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 0),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Text(
+                "Add some photos (optional)",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white),
+              )),
+          SizedBox(
+            height: 70,
+          ),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 100),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    )),
+              )),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 100),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    )),
+              )),
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 100),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Card(
+                elevation: 10,
+                child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Icon(
+                      Icons.add_a_photo,
+                      size: 40,
+                    )),
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _SubmitPage extends StatefulWidget {
+  _SubmitPage({Key? key}) : super(key: key);
+
+  @override
+  State<_SubmitPage> createState() => __SubmitPageState();
+}
+
+class __SubmitPageState extends State<_SubmitPage> {
+  ConfettiController cc = ConfettiController();
+  Widget _btnChild = Text("Submit",style: TextStyle(color: Colors.black, fontSize: 20));
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(colors: [
+            Color.fromARGB(255, 44, 143, 224),
+            Color.fromARGB(255, 18, 195, 226)
+          ]),
+        ),
+        padding: EdgeInsets.only(top: 100, left: 30, right: 30),
+        child: Column(
+  
+          children: [
+          ShowUpAnimation(
+              delayStart: Duration(milliseconds: 0),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: Text(
+                "That's all! Ready to submit your idea?",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                    color: Colors.white),
+              )),
+          SizedBox(height: 170,),
+          Stack(children: [
+             Positioned(
+            bottom: 20,
+            left: 55,
+            child: ConfettiWidget(
+              
+              displayTarget: false,
+              blastDirectionality: BlastDirectionality.explosive,
+              confettiController: cc,
+            ),
+          ),
+            ShowUpAnimation(
+              delayStart: Duration(milliseconds: 100),
+              animationDuration: Duration(milliseconds: 600),
+              curve: Curves.easeIn,
+              direction: Direction.horizontal,
+              offset: -0.2,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(140, 50)),
+                    backgroundColor: MaterialStateProperty.all(Colors.white)),
+                child: _btnChild,
+                onPressed: () async {
+                  setState(() {
+                    _btnChild = Center(child: CircularProgressIndicator(),);
+                  });
+                  await Future.delayed(Duration(seconds: 1)).then((_) {
+                    setState(() {
+                      _btnChild = Text("Submitted!",style: TextStyle(color: Colors.black, fontSize: 20));
+                    });
+                    cc.play();
+                  });
+                  
+                  },
+              )),
+             
+          ],)
+          
+          
+        ]));
   }
 }
