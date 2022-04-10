1088dcd6083deb2907d5120764a4076463c46409
Diyar Dogru <ddogru10@gmail.com>
2022-04-10 17:25:48 +0300

    unittest WIP

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,20 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ideabarrel/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    return;
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    //expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byWidget(MyApp()));
+    await tester.pump();
+
+    expect(find.text('1'), findsOneWidget);
   });
 }
