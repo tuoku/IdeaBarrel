b7e898959df00bb120d9a08bcf4612b71838b91e
Tuomas <tuomakuh@metropolia.fi>
2022-04-17 13:32:56 +0300

    multi image support

--- lib/ui/screens/swipe_screen.dart
+++ lib/ui/screens/swipe_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ideabarrel/repos/cosmos_repo.dart';
 import 'package:ideabarrel/ui/screens/idea_details_screen.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 
 class SwipeScreen extends StatefulWidget {
@@ -16,11 +17,14 @@ class _SwipeScreenState extends State<SwipeScreen> {
   var stackFinished = false;
   MatchEngine? engine;
   List<SwipeItem> ideas = [];
+  PageController _pc = PageController();
+  final _currentPageNotifier = ValueNotifier<int>(0);
+  double cardWidth = 0;
 
   @override
   void initState() {
     CosmosRepo().getAllIdeas().then((value) {
-      WidgetsBinding.instance!.addPostFrameCallback(
+      WidgetsBinding.instance.addPostFrameCallback(
         (timeStamp) {
           if (kDebugMode) print("Ideas fetched: ${value.length}");
           setState(() {
@@ -70,12 +74,23 @@ class _SwipeScreenState extends State<SwipeScreen> {
                             Expanded(
                               child: Stack(
                                 alignment: Alignment.bottomLeft,
-                                fit: StackFit.expand,
+                                fit: StackFit.loose,
                                 children: [
-                                  Image(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                          ideas[index].content['imgs'][0])),
+                                  PageView(
+                                    onPageChanged: (int index) {
+                                      _currentPageNotifier.value = index;
+                                    },
+                                    physics: NeverScrollableScrollPhysics(),
+                                    controller: _pc,
+                                    children: List.generate(
+                                        ideas[index].content['imgs'].length,
+                                        (i) {
+                                      return Image(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                              ideas[index].content['imgs'][i]));
+                                    }),
+                                  ),
                                   Positioned(
                                       bottom: 0,
                                       child: Container(
@@ -92,32 +107,115 @@ class _SwipeScreenState extends State<SwipeScreen> {
                                             MediaQuery.of(context).size.width,
                                         alignment: Alignment.bottomCenter,
                                       )),
-                                      LayoutBuilder(
-                                        
-                                        builder: ((context, constraints) {
-                                       return Container(
-                                          width: constraints.maxWidth,
-                                          padding: EdgeInsets.only(left:15,right: 10, bottom: 100),
-                                          child: Column(
-                                            mainAxisAlignment: MainAxisAlignment.end,
-                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                  LayoutBuilder(
+                                      builder: ((context, constraints) {
+                                        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                                          cardWidth = constraints.maxWidth;
+                                        },);
+                                    return Container(
+                                      width: constraints.maxWidth,
+                                      padding: EdgeInsets.only(
+                                          left: 15, right: 10, bottom: 100),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
                                         children: [
-                                          Text(ideas[index].content["title"], style: TextStyle(
-                                            fontSize: 24,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white
-                                          ),),
-                                          SizedBox(height: 10,),
-                                          Text(ideas[index].content["desc"],style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            
+                                          Text(
+                                            ideas[index].content["title"],
+                                            style: TextStyle(
+                                                fontSize: 24,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white),
                                           ),
-                                          maxLines: 8, overflow: TextOverflow.ellipsis,)
+                                          SizedBox(
+                                            height: 10,
+                                          ),
+                                          Text(
+                                            ideas[index].content["desc"],
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18,
+                                            ),
+                                            maxLines: 8,
+                                            overflow: TextOverflow.ellipsis,
+                                          )
                                         ],
-                                      ),);
-                                      }))
-                                      
+                                      ),
+                                    );
+                                  })),
+                                  Positioned(
+                                    top: 0,
+                                    right: 0,
+                                    width: cardWidth,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                          stops: [
+                                            0.1,
+                                            1.0
+                                          ],
+
+                                          colors: [
+                                            Color.fromARGB(71, 0, 0, 0),
+                                            Colors.transparent
+                                          ],
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter
+                                        )
+                                      ),
+                                      width: 100,
+                                      height: 50,
+                                      child: Align(
+                                        alignment: Alignment.topCenter,
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: CirclePageIndicator(
+                                            selectedDotColor: Colors.white,
+                                            dotColor: Colors.grey,
+                                            itemCount: ideas[index]
+                                                .content['imgs']
+                                                .length,
+                                            currentPageNotifier:
+                                                _currentPageNotifier,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ),
+                                  Positioned(
+                                    right: 0,
+                                    bottom: 330,
+                                    child: Container(
+                                      height: 400,
+                                      width: 150,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          _pc.nextPage(
+                                              duration:
+                                                  Duration(milliseconds: 100),
+                                              curve: Curves.easeIn);
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    left: 0,
+                                    bottom: 330,
+                                    child: Container(
+                                      height: 400,
+                                      width: 150,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          _pc.previousPage(
+                                              duration:
+                                                  Duration(milliseconds: 100),
+                                              curve: Curves.easeIn);
+                                        },
+                                      ),
+                                    ),
+                                  ),
                                 ],
                               ),
                             )
