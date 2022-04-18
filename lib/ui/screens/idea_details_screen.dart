45eff5975800b7f64bbb99a93d8bd06a2eddc822
Tuomas <tuomakuh@metropolia.fi>
2022-04-18 15:30:27 +0300

    improved idea details screen

--- lib/ui/screens/idea_details_screen.dart
+++ lib/ui/screens/idea_details_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
 class IdeaDetailsScreen extends StatefulWidget {
-  const IdeaDetailsScreen({Key? key, required this.pageView, required this.pageViewTag})
+  const IdeaDetailsScreen(
+      {Key? key,
+      required this.pageView,
+      required this.pageViewTag,
+      required this.title,
+      required this.titleTag,
+      required this.pageController,
+      required this.pageNotifier,
+      required this.initialIndex,
+      required this.urls,
+      required this.titleString,
+      required this.descTag,
+      required this.descString})
       : super(key: key);
 
   final String pageViewTag;
   final Widget pageView;
+  final ValueNotifier<int> pageNotifier;
+  final PageController pageController;
+  final int initialIndex;
+  final List<String> urls;
+
+  final String titleTag;
+  final Widget title;
+  final String titleString;
+
+  final String descTag;
+  final String descString;
+
   @override
   State<IdeaDetailsScreen> createState() => _IdeaDetailsScreenState();
 }
@@ -13,68 +37,142 @@ class IdeaDetailsScreen extends StatefulWidget {
 class _IdeaDetailsScreenState extends State<IdeaDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: ListView(children: [
-          Container(height: 500,child:widget.pageView ,)
-      ,
-      const Text(
-        "  Comments",
-        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-      ),
-      const SizedBox(
-        height: 15,
-      ),
-      Row(
-        children: const [
-          SizedBox(
-            width: 10,
-          ),
-          CircleAvatar(
-            backgroundImage: AssetImage("assets/ukko1.jpeg"),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text("Hyvä idea!!!!!!!")
-        ],
-      ),
-      const SizedBox(
-        height: 20,
-      ),
-      Row(
-        children: const [
-          SizedBox(
-            width: 10,
-          ),
-          CircleAvatar(
-            backgroundImage: AssetImage("assets/ukko2.jpeg"),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text("Uijuma")
-        ],
-      ),
-      const SizedBox(
-        height: 20,
-      ),
-      Row(
-        children: const [
-          SizedBox(
-            width: 10,
-          ),
-          CircleAvatar(
-            backgroundImage: AssetImage("assets/ukko3.jpeg"),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text("😍😍😍😍😍😍😍😍")
-        ],
-      ),
-      const TextField(
-        decoration: InputDecoration(labelText: "Write a comment"),
-      ),
-    ]));
+    return Scaffold(
+        body: CustomScrollView(
+            physics:
+                BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            slivers: [
+          SliverAppBar(
+              stretch: true,
+              pinned: true,
+              floating: false,
+              snap: false,
+              expandedHeight: 500,
+              collapsedHeight: kToolbarHeight,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: false,
+                  stretchModes: [StretchMode.zoomBackground],
+                  title: Hero(
+                      child: Material(
+                          type: MaterialType.transparency,
+                          child: Text(widget.titleString,
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white))),
+                      tag: widget.titleTag),
+                  background: Container(
+                    
+                    decoration: BoxDecoration(color: Colors.black,),
+                      height: 500,
+                      child: Hero(
+                          child: Material(
+                              type: MaterialType.transparency,
+                              child: PageView(
+                            onPageChanged: (int index) {
+                              setState(() {
+                                widget.pageNotifier.value = index;
+                                widget.pageController.jumpToPage(index);
+                              });
+                            },
+                            physics: const BouncingScrollPhysics(
+                                parent: AlwaysScrollableScrollPhysics()),
+                            controller: PageController(
+                                initialPage: widget.initialIndex),
+                            children: List.generate(widget.urls.length, (i) {
+                              return ShaderMask(
+                                  shaderCallback: (rect) {
+                                    return LinearGradient(
+                                      begin: Alignment.center,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        Colors.black,
+                                        Colors.transparent
+                                      ],
+                                    ).createShader(Rect.fromLTRB(
+                                        0, 250, rect.width, rect.height));
+                                  },
+                                  blendMode: BlendMode.dstIn,
+                                  child: Image(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(widget.urls[i])));
+                            }),
+                          )),
+                          tag: widget.pageViewTag)))),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Hero(
+                      tag: widget.descTag,
+                      child: Material(
+                          child: Text(
+                        widget.descString,
+                        style: TextStyle(fontSize: 18),
+                      )))),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Comments",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+              Row(
+                children: const [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/ukko1.jpeg"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Hyvä idea!!!!!!!")
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/ukko2.jpeg"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Uijuma")
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/ukko3.jpeg"),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("😍😍😍😍😍😍😍😍")
+                ],
+              ),
+              SizedBox(
+                height: 500,
+              )
+            ]),
+          )
+        ]));
   }
 }
