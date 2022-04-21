dde38e80039ae7086a1aef82d6c6fa58824292ee
Diyar Dogru <ddogru10@gmail.com>
2022-04-21 03:23:46 +0300

    Merge https://github.com/tuoku/IdeaBarrel into unittest

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,15 +9,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ideabarrel/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    //expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
 
-    await tester.tap(find.byWidget(MyApp()));
+    await tester.tap(find.byWidget(const MyApp()));
     await tester.pump();
 
     expect(find.text('1'), findsOneWidget);
